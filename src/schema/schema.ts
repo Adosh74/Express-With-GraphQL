60b0a1c1ebd336defe3e67baca8a869f6abd7b5f
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-11-08 20:24:25 +0200

    wip

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,5 +1,24 @@
-import { GraphQLID, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import {
+	GraphQLID,
+	GraphQLList,
+	GraphQLObjectType,
+	GraphQLSchema,
+	GraphQLString,
+} from 'graphql';
 import { projects, clients } from '../sampleData';
+
+// project type
+const ProjectType = new GraphQLObjectType({
+	name: 'Project',
+	description: 'Project Type',
+	fields: () => ({
+		id: { type: GraphQLID },
+		// clientId: { type: GraphQLID },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+		status: { type: GraphQLString },
+	}),
+});
 
 // client type
 const ClientType = new GraphQLObjectType({
@@ -17,12 +36,32 @@ const RootQuery = new GraphQLObjectType({
 	name: 'RootQueryType',
 	description: 'Root Query',
 	fields: {
+		clients: {
+			type: new GraphQLList(ClientType),
+			resolve(parent, args) {
+				return clients;
+			},
+		},
 		client: {
 			type: ClientType,
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				// code to get data from db / other source
 				return clients.find((client) => client.id === args.id);
+			},
+		},
+		projects: {
+			type: new GraphQLList(ProjectType),
+			resolve(parent, args) {
+				return projects;
+			},
+		},
+		project: {
+			type: ProjectType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				// code to get data from db / other source
+				return projects.find((project) => project.id === args.id);
 			},
 		},
 	},
